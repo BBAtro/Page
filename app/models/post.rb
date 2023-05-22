663ca432ff33749ca006cd91608b9aded0c48fab
Bekkalie <drerzhik@mail.ru>
2023-05-23 02:16:54 +0600

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3}
+  validates :body, presence: true, length: {minimum: 5}
+
+  def formatted_created_at
+    created_at.strftime('%Y.%m.%d %H:%M')
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'pages#index'
+
+  resources :posts, only: %i[index new create]
+
+end
