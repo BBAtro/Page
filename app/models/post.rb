2fa89b56e069b74f2465feff9d86e13947ed1ec0
Bekkalie <drerzhik@mail.ru>
2023-06-10 01:58:14 +0600

    Little stylesheet

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: {minimum: 5}
 
   def formatted_created_at
-    created_at.strftime('%Y.%m.%d %H:%M')
+    created_at.strftime('%H:%M %d.%m.%Y')
   end
 
 end
